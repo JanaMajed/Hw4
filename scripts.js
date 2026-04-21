f79f9fd064637f74aa37343a62553e8cc99b62e8
Jana Majed Alamer <janamijad@gmail.com>
2026-04-22 00:26:48 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,30 +1,25 @@
 function showLoadTime() {
-    console.log("Function is working!");
-
     var footer = document.querySelector("footer");
 
-    var newParagraph = document.createElement("p");
+    if (footer) {
+        var newParagraph = document.createElement("p");
+        var currentDate = new Date();
+        var text = document.createTextNode(
+            "Page loaded on: " + currentDate.toLocaleString("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                second: "numeric",
+                hour12: true
+            })
+        );
 
-    var currentDate = new Date();
-var text = document.createTextNode(
-    "Page loaded on: " + currentDate.toLocaleString("en-US", {
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: true
-    })
-);
-    newParagraph.appendChild(text);
-
-    footer.appendChild(newParagraph);
+        newParagraph.appendChild(text);
+        footer.appendChild(newParagraph);
+    }
 }
-
-window.onload = showLoadTime;
-
-
 
 function setupBlockquoteEffect() {
     var quote = document.getElementById("quote");
@@ -41,3 +36,8 @@ function setupBlockquoteEffect() {
         });
     }
 }
+
+window.onload = function () {
+    showLoadTime();
+    setupBlockquoteEffect();
+};
